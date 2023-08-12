503f4c9c1c2c1e650e10168108856e0a07c91379
Riddhick <riddhick14@gmail.com>
2023-08-12 22:17:30 +0530

    Sticky Navbar & Large Screen Project card modified

--- Portfolio/src/Cards.jsx
+++ Portfolio/src/Cards.jsx
@@ -4,35 +4,42 @@ import {BsChevronCompactLeft,BsChevronCompactRight} from "react-icons/bs";
 
 export default function Cards(prpos){
     const [getProject,setProject]=useState(0);
-
+    const [getleft,setleft]=useState(1);
+    const [getright,setRight]=useState(1);
     const nextSlide=()=>{
         const pos=getProject;
         const newPos=(pos+1)%prpos.card.length;
         setProject(newPos)
+        setRight((newPos+1)%prpos.card.length)
         console.log(newPos)
     }
 
     const prevSlide=()=>{
         const pos=getProject;
-        const newPos=(pos+1)%prpos.card.length;
+        var newPos=(pos+1)%prpos.card.length;
         setProject(newPos)
+        setleft((newPos+1)%prpos.card.length)
         console.log(newPos)
     }
     return(
-        <div className=" w-full flex flex-row justify-center gap-2 lg:items-center" >
-                <div className="w-1/12 pt-32 md:pt-64  lg:w-1/3 lg:pt-24 lg:rounded-[20px_20px_20px_20px] lg:bg-gradient-to-r from-zinc-900 to-zinc-700 lg:h-52"><BsChevronCompactLeft onClick={prevSlide} size={30}/></div>
-                <div className="bg-black w-10/12 h-72  md:h-[550px] lg:h-[450px] lg:w-1/3 rounded-[20px] flex flex-col gap-0">
+        <div className=" w-full flex flex-row justify-center gap-2 lg:items-center lg:w-2/3" >
+                <div className="w-1/12 pt-32 md:pt-64 lg:h-[300px] lg:w-1/3 lg:pt-0 lg:rounded-[20px_20px_20px_20px] lg:bg-gradient-to-r from-zinc-900">
+                    <img className="h-0 w-0 lg:h-[300px] lg:w-full lg:rounded-[20px_20px_20px_20px] lg:opacity-5" src={prpos.card[getleft].image} onClick={prevSlide}></img>
+                    <BsChevronCompactLeft className="lg:w-0" onClick={prevSlide} size={30}/></div>
+                <div className="bg-black w-10/12 h-72  md:h-[550px] lg:h-[350px] lg:w-1/3 rounded-[20px] flex flex-col gap-0">
                     <div className="bg-black rounded-[20px_20px_0px_0px]">
                         <img className="rounded-[20px_20px_0px_0px] md:h-fit md:w-full" src={prpos.card[getProject].image}></img>
                     </div>
-                    <div className="text-center py-4 md:py-10 md:text-2xl md:font-medium">{prpos.card[getProject].name}</div>
-                    <div className="grid grid-cols-3 gap-2 md:gap-4 px-6  md:px-4 text-xs text-black font-medium md:text-lg md:font-semibold">
+                    <div className="text-center py-4 md:py-10 md:text-2xl md:font-medium lg:text-sm lg:py-6">{prpos.card[getProject].name}</div>
+                    <div className="grid grid-cols-3 gap-2 md:gap-4 px-6  md:px-4 text-xs text-black font-medium md:text-lg md:font-semibold lg:text-sm">
                         {prpos.card[getProject].tech.map(t=>(
-                            <div className="bg-amber-400 w-11/12 md:h-8 text-center  rounded-full">{t}</div>
+                            <div className="bg-amber-400 w-11/12 md:h-8 lg:h-[20px] text-center  rounded-full">{t}</div>
                         ))}
                     </div>
                 </div>
-                <div className="w-1/12 pt-32 md:pt-64 pl-2 md:pl-4 lg:pt-24 lg:pl-96 lg:w-1/3 lg:rounded-[20px_20px_20px_20px] lg:bg-gradient-to-r from-zinc-700 to-zinc-900 lg:h-52 lg:pr-8"><BsChevronCompactRight onClick={nextSlide} size={30}/></div>   
+                <div className="w-1/12 pt-32 md:pt-64 pl-2 md:pl-4 lg:pt-0 lg:pl-0 lg:w-1/3 lg:rounded-[20px_20px_20px_20px] lg:bg-gradient-to-r from-zinc-900 to-zinc-900 lg:h-[300px] lg:pr-8">
+                <img className="h-0 w-0 lg:h-[300px] lg:w-full lg:rounded-[20px_20px_20px_20px] lg:opacity-5" src={prpos.card[getright].image} onClick={nextSlide}></img>
+                    <BsChevronCompactRight className="lg:w-0" onClick={nextSlide} size={30}/></div>   
             </div> 
     )
 }
--- Portfolio/src/Navbar.jsx
+++ Portfolio/src/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar(){
     return (
-        <div className="py-4 px-10 lg:px-20 w-full flex flex-row text-gray-50 justify-between">
+        <div className="py-4 bg-zinc-900 px-10 lg:px-20 w-full flex flex-row text-gray-50 justify-between sticky top-0">
             <div className="underline underline-offset-16 decoration-amber-400 decoration-4 font-medium" >HOME</div>
             <div className="flex flex-row space-x-8">
                 <div className="">Projects</div>
