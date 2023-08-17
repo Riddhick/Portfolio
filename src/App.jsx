f864da52bbf443f32e38a95801217d0ea1876039
Riddhick <riddhick14@gmail.com>
2023-08-17 19:38:37 +0530

    Added: Framer Motion animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import {useRef,useEffect} from "react";
+import { useInView,useAnimation } from "framer-motion";
+import {motion} from "framer-motion";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Projects from "./Projects";
 import Contact from "./Contact";
 
 function App() {
- 
+  const myref=useRef(null);
+  const isInview=useInView(myref,{once:false})
+  const animation=useAnimation()
+
+  useEffect(()=>{
+    isInview?animation.start({opacity:1,trasition:{type:'tween',duration:0.3}}): animation.start({ opacity:0})
+  },[isInview])
+
   return (
     <>
     <div className="lg:px-10">
       <Navbar/>
-     <section id="home"> <Home /></section>
+     <section id="home" > <Home /></section>
      <section id="projects"><Projects/></section>
       
     </div >
-    <section id="contact">
+    <motion.section id="contact" ref={myref} animate={animation}>
     <Contact />
-    </section>
+    </motion.section>
     </>
   )
     
