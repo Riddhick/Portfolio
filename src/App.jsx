5bbeede340ad7b7dfec094233a7e6e94a342ceab
Riddhick <riddhick14@gmail.com>
2023-08-17 11:51:58 +0530

    Navbar button functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Projects from "./Projects";
 import Contact from "./Contact";
+
 function App() {
  
   return (
     <>
     <div className="lg:px-10">
-     <Navbar/>
-      <Home />
-      <Projects/>
+      <Navbar/>
+     <section id="home"> <Home /></section>
+     <section id="projects"><Projects/></section>
       
-    </div>
+    </div >
+    <section id="contact">
     <Contact />
+    </section>
     </>
   )
     
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { HashLink as Link} from "react-router-hash-link";
+import {BrowserRouter } from "react-router-dom";
 
 export default function Navbar(){
     return (
-        <div className="py-4 bg-zinc-900 px-10 lg:px-20 w-full flex flex-row text-gray-50 justify-between sticky top-0">
-            <div className="underline underline-offset-16 decoration-amber-400 decoration-4 font-medium" >HOME</div>
+        <div className="py-4 bg-zinc-900 px-10 lg:px-32 w-full flex flex-row text-gray-50 justify-between sticky top-0">
+            <div className="underline underline-offset-16 decoration-amber-400 decoration-4 font-medium" ><Link smooth to="#home" >HOME</Link></div>
             <div className="flex flex-row space-x-8">
-                <div className="">Projects</div>
-                <div className="">Contact</div>
+                <div className="hover:underline underline-offset-16 decoration-amber-400 decoration-4"><Link smooth to="#projects" >Projects</Link></div>
+                <div className="hover:underline underline-offset-16 decoration-amber-400 decoration-4"><Link smooth to="#contact" >Contact</Link></div>
             </div>
         </div>
     )
